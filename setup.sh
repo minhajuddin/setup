c40f2e623bf9899319ca87d350ff061e64f108a5
Khaja Minhajuddin <minhajuddin.k@gmail.com>
2012-05-04 11:31:42 +0530

    updated for 12.04

--- setup.sh
+++ setup.sh
@@ -7,24 +7,13 @@ sudo apt-get dist-upgrade -y
 sudo apt-get upgrade -y
 
 #install required software
-sudo apt-get install openssh-server vim-gnome curl git-core bison build-essential zlib1g-dev libssl-dev libreadline6-dev libxml2-dev autoconf libxslt1-dev libpq-dev postgresql mutt parcellite alarm-clock gtimelog gnome-do emacs23 irssi shutter gimp deluge vlc htop pidgin ctags gitk git-gui vifm libgmp3c2 libgmpxx4ldbl libgmp3-dev freeglut3 freeglut3-dev libxrandr-dev X11-xserver-utils -y
+sudo apt-get install openssh-server vim-gnome curl git-core bison build-essential zlib1g-dev libssl-dev libreadline6-dev libxml2-dev autoconf libxslt1-dev libpq-dev postgresql mutt parcellite alarm-clock gtimelog irssi shutter gimp deluge vlc htop pidgin ctags gitk git-gui vifm libgmp3c2 libgmpxx4ldbl libgmp3-dev imagemagick libmagickcore-dev libmagickcore4 libmagickcore4-extra libmagickwand-dev libmagickwand4 -y
 
-echo "install ghc using source"
+echo "install ghc using source, if you need haskell"
 
 #install rvm
-bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
-#tweak bashrc
-mkdir ~/archives
-mv ~/.bashrc ~/archives/
+curl -L get.rvm.io | bash -s stable
+
 #install ruby 1.9.2
 exec bash
 rvm install 1.9.2
-rvm --default 1.9.2
-rvm use 1.9.2
-
-#clone dotfiles repo
-mkdir ~/repos
-cd ~/repos
-git clone git://github.com/minhajuddin/dotfiles.git
-cd ~/repos/dotfiles && ruby setup.rb #TODO: change the setup.rb file to setup.sh
-
