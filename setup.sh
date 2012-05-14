117f8e40cfb19e2f0fa3ccef66c9ff701cc5e322
Khaja Minhajuddin <minhajuddin.k@gmail.com>
2012-05-14 19:16:20 +0530

    tweaks to get rmagick working

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo apt-get dist-upgrade -y
 sudo apt-get upgrade -y
 
 #install required software
-sudo apt-get install openssh-server vim-gnome curl git-core bison build-essential zlib1g-dev libssl-dev libreadline6-dev libxml2-dev autoconf libxslt1-dev libpq-dev postgresql mutt parcellite alarm-clock gtimelog irssi shutter gimp deluge vlc htop pidgin ctags gitk git-gui vifm libgmp3c2 libgmpxx4ldbl libgmp3-dev imagemagick libmagickcore-dev libmagickcore4 libmagickcore4-extra libmagickwand-dev libmagickwand4 -y
+sudo apt-get install openssh-server vim-gnome curl git-core bison build-essential zlib1g-dev libssl-dev libreadline6-dev libxml2-dev autoconf libxslt1-dev libpq-dev postgresql mutt parcellite alarm-clock gtimelog irssi shutter gimp deluge vlc htop pidgin ctags gitk git-gui vifm libgmp3c2 libgmpxx4ldbl libgmp3-dev imagemagick libmagickcore-dev libmagickcore4 libmagickcore4-extra libmagickwand-dev libmagickwand4 libmagick9-dev graphicsmagick-libmagick-dev-compat -y
 
 echo "install ghc using source, if you need haskell"
 
